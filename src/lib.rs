cdaa0c9796c5af8fbd31b0b1298717503e610b1a
Stefan Ulbrich <6009224+StefanUlbrich@users.noreply.github.com>
2024-11-24 11:39:00 +0100

    Add a little text to the crate's docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,21 @@
-//! ## Naming conventions
+//! ## About
 //!
+//! **Warning: still under heavy development**
+//!
+//! This crate contains methods for kinematics and rigging, and dynamics in biomechanics applications
+//! and animation. It contains data structures and visitor algorithms for handling tree structures, and
+//! will support multiple algebra packages (planned: [ndarray](https://github.com/rust-ndarray/ndarray),
+//! [ngalgebra](https://github.com/dimforge/nalgebra), [faer](https://github.com/sarah-ek/faer-rs),
+//! [glam](https://github.com/bitshifter/glam-rs))
+//!
+//! See the [Mannequin] struct to get started.
+//!
+//! ## Reading list
+//!
+//! * [Lecture on Inverse Kinematics](https://cseweb.ucsd.edu/classes/wi17/cse169-a/sessions.html)
+//! * [Dynamics in Cumputer Graphics](https://foswiki.cs.rpi.edu/foswiki/pub/RoboticsWeb/LabPublications/BETCstar_part1.pdf)
+//!
+//! ## Naming conventions
 //! * Traits – adjectives that indicate capability and behavior (TODO this is currently not always true)
 //! * Structs – substantives that indicate entities implementing a behavior
 //! * Methods – imperative forms with the exception of getters and factories, which
