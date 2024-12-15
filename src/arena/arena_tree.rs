1da882ac1fd6fbdaa18ed312ba7ec8ba7d8ed8da
Stefan Ulbrich <6009224+StefanUlbrich@users.noreply.github.com>
2024-12-15 11:38:26 +0100

    Split arena tree file

--- src/arena/arena_tree.rs
+++ src/arena/arena_tree.rs
@@ -1,4 +1,4 @@
-/*! Implementation of a tree iteration with [arena allocation](https://en.wikipedia.org/wiki/Region-based_memory_management) */
+use core::fmt;
 use itertools::Itertools;
 
 use crate::{
@@ -7,81 +7,15 @@ use crate::{
     Order::{self, BreadthFirst, DepthFirst},
     TreeIterable,
 };
-use core::fmt;
 
-/// Iterator for a depth-first iteration when the data is not already sorted accordingly
-pub struct DepthFirstIterator<'a, 'b, T>
-where
-    'a: 'b,
-    T: 'static + fmt::Debug + PartialEq,
-{
-    tree: &'a ArenaTree<T>,
-    stack: Vec<std::slice::Iter<'b, <ArenaTree<T> as TreeIterable<T>>::NodeRef>>,
-    // root: &'b Vec<<ArenaTree<T> as IterableTree>::NodeRef>,
-    // root2: Vec<<ArenaTree<T> as IterableTree>::NodeRef>,
-    // stack2: Vec<std::slice::Iter<'b, <ArenaTree<T> as IterableTree>::NodeRef>>,
-}
-
-impl<'a, 'b, T> DepthFirstIterator<'a, 'b, T>
-where
-    T: 'static + fmt::Debug + PartialEq,
-{
-    // TODO did not manage to over write the root nodes :(
-    pub fn new(tree: &'a ArenaTree<T>, root: &'b [<ArenaTree<T> as TreeIterable<T>>::NodeRef]) -> Self {
-        let mut stack = Vec::with_capacity(tree.max_depth);
-        stack.push(root.iter());
-        println!("Creating new depth-first iterator (slow)");
-        DepthFirstIterator { tree, stack }
-    }
-}
-impl<'a, 'b, T> Iterator for DepthFirstIterator<'a, 'b, T>
-where
-    T: fmt::Debug + PartialEq,
-{
-    type Item = &'a ArenaNode<T>;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        if let Some(last) = self.stack.last_mut() {
-            if let Some(child_ref) = last.next() {
-                let node = &self.tree.nodes[*child_ref];
-                self.stack.push(node.children.iter());
-                Some(&self.tree.nodes[*child_ref])
-            } else {
-                self.stack.pop();
-                self.next()
-            }
-        } else {
-            None
-        }
-    }
-}
-
-/// Iterator for a breadth-first iteration when the data is not already sorted accordingly
-
-struct BreadthFirstIterator<'a, T> {
-    tree: &'a ArenaTree<T>,
-}
-
-impl<'a, T> BreadthFirstIterator<'a, T> {
-    pub fn new(tree: &'a ArenaTree<T>) -> Self {
-        BreadthFirstIterator { tree }
-    }
-}
-impl<'a, T> Iterator for BreadthFirstIterator<'a, T> {
-    type Item = &'a ArenaNode<T>;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        todo!()
-    }
-}
-
+use super::{BreadthFirstIterator, DepthFirstIterator};
 /// A node structure to be used in an arena allocated tree. Fields are used to speed up iteration
 #[derive(Debug)]
 pub struct ArenaNode<T> {
     load: T,
     /// TODO check if we need this?
     node_ref: usize,
-    children: Vec<usize>,
+    pub(crate) children: Vec<usize>,
     /// Only used when data is sorted as it is traversed
     width: usize,
     /// Only used in depth-first interation ([DepthFirstIterator])
@@ -157,7 +91,7 @@ pub struct ArenaTree<T> {
     sorting: Option<Order>,
 
     /// Memory allocated area for nodes
-    nodes: Vec<ArenaNode<T>>,
+    pub(crate) nodes: Vec<ArenaNode<T>>,
 
     /// Caches the sequence of iteration (depth-first)
     depth_first_cache: Option<Vec<usize>>,
@@ -168,7 +102,7 @@ pub struct ArenaTree<T> {
     roots: Vec<usize>,
 
     /// Maximal recursion depth of the dree
-    max_depth: usize,
+    pub(crate) max_depth: usize,
 }
 
 impl<T> ArenaTree<T> {
---
+++ src/arena/mod.rs
@@ -0,0 +1,5 @@
+pub mod arena_tree;
+pub mod iterators;
+
+pub use arena_tree::{ArenaNode, ArenaTree};
+pub use iterators::{BreadthFirstIterator, DepthFirstIterator};
