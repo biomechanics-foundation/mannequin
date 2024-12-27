a0601c8ff3f3cf434ba43e4d7445ac0ca253a5fb
Stefan Ulbrich <6009224+StefanUlbrich@users.noreply.github.com>
2024-12-27 17:27:51 +0100

    move iterator method into the correct file

--- src/arena/iterators.rs
+++ src/arena/iterators.rs
@@ -1,7 +1,7 @@
 /*! Implementation of a tree iteration with [arena allocation](https://en.wikipedia.org/wiki/Region-based_memory_management) */
 use super::arena_tree::{ArenaNode, ArenaTree};
 
-use crate::TreeIterable;
+use crate::{Nodelike, TreeIterable};
 use core::fmt;
 
 /// Iterator for a depth-first iteration when the data is not already sorted accordingly
@@ -67,5 +67,50 @@ impl<'a, T> Iterator for BreadthFirstIterator<'a, T> {
 
     fn next(&mut self) -> Option<Self::Item> {
         todo!()
+    }
+}
+
+/// Accumulation over a *Depth First* iteration (this is not checked).
+pub trait DepthFirstAccumulation<'a, Node, T, F, Parameter, Accumulated>
+where
+    Node: Nodelike<T> + 'a,
+    F: Fn(&Node, &Parameter, &Accumulated) -> Accumulated + 'static,
+    Accumulated: Clone,
+{
+    /// Method on an iterator. Accepts a slice of parameters used to
+    /// compute a value that can be accumulated considering the depth of the tree.
+    /// The latter means that each node receives the accumulated values from its parent.
+    /// The calculation of the value and the accumulation is specified by the `accumulator`
+    /// parameter (usually, a closure). Accumulation requires a `first` element (neutral element)
+    /// and a max depth (for performance)
+    fn accumulate(
+        self,
+        param: &[Parameter],
+        accumulator: F,
+        first: Accumulated,
+        max_depth: usize,
+    ) -> impl Iterator<Item = (&'a Node, Accumulated)>;
+}
+
+impl<'a, 'b, N, T, F, P, R> DepthFirstAccumulation<'a, N, T, F, P, R> for Box<dyn Iterator<Item = &'a N> + 'b>
+where
+    N: Nodelike<T> + 'a,
+    F: Fn(&N, &P, &R) -> R + 'static,
+    R: Clone,
+{
+    fn accumulate(self, param: &[P], acc: F, first: R, max_depth: usize) -> impl Iterator<Item = (&'a N, R)> {
+        self.into_iter()
+            .zip(param.iter())
+            // TODO look up whether the move here (required for the acc parameter) slows down execution
+            // TODO this function should replace the accumulation in `rigid.rs`
+            .scan(Vec::<R>::with_capacity(max_depth), move |stack, (node, param)| {
+                let depth = stack.len();
+                while node.depth() < depth {
+                    stack.pop();
+                }
+                let result = acc(node, param, stack.last().unwrap_or(&first));
+                stack.push(result.clone());
+                Some((node, result))
+            })
     }
 }
