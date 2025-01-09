e0875520212a93f6dcbc3e0d736fd273f291d71b
Stefan Ulbrich <6009224+StefanUlbrich@users.noreply.github.com>
2025-01-09 23:19:07 +0100

    Propose simplified interface for trees

--- src/iterable_tree.rs
+++ src/iterable_tree.rs
@@ -45,20 +45,40 @@ pub trait Nodelike<Load, NodeId> {
     // Therefor this has been (will be) added there
 }
 
-/// A datastructure to hold a tree hierarchy of data contained in `NodeLike`s.
-///
-/// TODO Put an emphasis on that mutability is tough. after the tree has been constructed
-/// it is advised to not access it in a mutable way (because of borrowed node references)
-pub trait TreeIterable<Load, NodeId>
+trait BaseDirectionIterable<Load, NodeId>
 where
     Load: PartialEq,
     NodeId: Eq + Clone + Hash + Debug,
 {
     type Node: 'static + Nodelike<Load, NodeId> + Debug; // cannot hold references
 
-    fn iter<'a, 'b>(&'a self, traversal: Order, root: Option<&Self::Node>) -> impl Iterator<Item = &'a Self::Node>
-    where
-        'a: 'b;
+    fn root(&self) -> Result<&Self::Node, MannequinError<NodeId>>;
+    fn children(&self, node: &Self::Node) -> Result<Vec<&Self::Node>, MannequinError<NodeId>>;
+    fn node_by_load(&self, load: &Load) -> Option<&Self::Node>;
+    fn node_by_id(&self, node_id: &NodeId) -> Option<&Self::Node>;
+}
+
+/// A datastructure to hold a tree hierarchy of data contained in `NodeLike`s.
+///
+/// TODO Put an emphasis on that mutability is tough. after the tree has been constructed
+/// it is advised to not access it in a mutable way (because of borrowed node references)
+pub trait DirectionIterable<Load, NodeId>: BaseDirectionIterable<Load, NodeId>
+where
+    Load: PartialEq,
+    NodeId: Eq + Clone + Hash,
+{
+    fn iter_depth(&self) -> impl Iterator<Item = &Self::Node>;
+    fn iter_depth_sub(&self, root: &Self::Node) -> impl Iterator<Item = &Self::Node>;
+
+    fn iter_breadth(&self) -> impl Iterator<Item = &Self::Node>;
+    fn iter_breadth_sub(&self, root: &Self::Node) -> impl Iterator<Item = &Self::Node>;
+
+    // Leave in for historic reasons .. until documented how to return Boxed iterators
+    // fn iter<'a, 'b>(&'a self, traversal: Order, root: Option<&Self::Node>) -> impl Iterator<Item = &'a Self::Node>
+    // where
+    //     'a: 'b;
+
+    // Modifying methods
 
     /// Add a new node to the tree. A tree can have multiple root nodes; their parents are `None`
     fn add(&mut self, load: Load, node_ref: NodeId, parent: &NodeId) -> Result<NodeId, MannequinError<NodeId>>;
@@ -66,13 +86,35 @@ where
     /// Deletes all nodes and sets a new root
     fn set_root(&mut self, root_load: Load, root_ref: NodeId) -> NodeId;
 
-    /// Optimizes the tree for a traversal order after all nodes have been added. May also
-    /// generate caches if applicable.
-    /// **Warning** breaks all existing references in your program!
-    fn optimize(&mut self, for_traversal: Order);
+    /// Generate optimized
+    fn to_depth_first(self) -> impl DepthFirstIterable<Load, NodeId>;
+    fn to_breadth_first(self) -> impl BreadthFirstIterable<Load, NodeId>;
+}
 
-    fn root(&self) -> Result<&Self::Node, MannequinError<NodeId>>;
-    fn children(&self, node: &Self::Node) -> Result<Vec<&Self::Node>, MannequinError<NodeId>>;
-    fn node_by_load(&self, load: &Load) -> Option<&Self::Node>;
-    fn node_by_id(&self, node_id: &NodeId) -> Option<&Self::Node>;
+pub trait OptimizedDirectionIterable<Load, NodeId>: BaseDirectionIterable<Load, NodeId>
+where
+    Load: PartialEq,
+    NodeId: Eq + Clone + Hash,
+{
+    fn iter(&self) -> impl Iterator<Item = &Self::Node>;
+    fn iter_mut(&self) -> impl Iterator<Item = &mut Self::Node>;
+    fn iter_sub(&self, root: &Self::Node) -> impl Iterator<Item = &Self::Node>;
+    // That one maybe not :-(
+    fn iter_sub_mut(&self, root: &Self::Node) -> impl Iterator<Item = &mut Self::Node>;
+}
+
+pub trait DepthFirstIterable<Load, NodeId>: OptimizedDirectionIterable<Load, NodeId>
+where
+    Load: PartialEq,
+    NodeId: Eq + Clone + Hash,
+{
+    fn test() -> Self::Node;
+}
+
+pub trait BreadthFirstIterable<Load, NodeId>: OptimizedDirectionIterable<Load, NodeId>
+where
+    Load: PartialEq,
+    NodeId: Eq + Clone + Hash,
+{
+    // Either copy or make extending traits with no additional methods
 }
