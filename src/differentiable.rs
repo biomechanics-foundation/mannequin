e74c7fdd93d78129a295187237ffd449b9287c9d
Stefan Ulbrich <6009224+StefanUlbrich@users.noreply.github.com>
2025-01-10 22:36:06 +0100

    Raw implementation of the generic jacobian

--- src/differentiable.rs
+++ src/differentiable.rs
@@ -1,11 +1,11 @@
-use crate::{accumulate, rigid, DepthFirst, Nodelike, Rigid, TransformationAccumulation, TreeIterable};
+use crate::{DepthFirst, Nodelike, Rigid, TransformationAccumulation, TreeIterable};
 use itertools::{izip, Itertools};
 #[cfg(feature = "rayon")]
 use rayon::prelude::*;
-use std::hash::Hash;
+use std::{collections::HashSet, hash::Hash};
 
 // TODO maybe make precision generic
-trait Differentiable<T, R, I>
+pub trait Differentiable<T, R, I>
 where
     T: TreeIterable<R, I>,
     R: Rigid,
@@ -17,18 +17,86 @@ where
     fn jacobian(&self) -> &Self::Data;
 
     /// compute the jacobian matrix
-    fn compute(&mut self, tree: &T, params: &[R::Parameter], active_joints: &[bool], active_points: &[bool]) {
-        // selected_joints: &HashSet<String>,
-        // selected_points: &HashSet<String>,
-        // let active_joints = tree
-        //     .iter(DepthFirst, None)
-        //     .map(|n| selected_joints.get(&n.id()).is_some())
-        //     .collect_vec();
-        // let active_points = tree
-        //     .iter(DepthFirst, None)
-        //     .map(|n| selected_points.get(&n.id()).is_some())
-        //     .collect_vec();
+    fn setup(&mut self, tree: &T, active_joints: &HashSet<I>, active_points: &HashSet<I>);
+    fn compute(&mut self, tree: &T, params: &[R::Parameter]); //, active_joints: &[bool], active_points: &[bool]);
 
+    /// provide a reference to the raw data and the dimensions of its axes (so first the major axis)
+    fn data(&mut self) -> &mut [f64];
+    fn rows(&self) -> usize;
+    fn cols(&self) -> usize;
+}
+
+/// Base implementation that should be used in backend implementation as a composite
+/// Call methods in children like that: `<VecJacobian as Differentiable<T, R, I>>::compute`
+#[derive(Debug, Default)]
+pub struct VecJacobian {
+    data: Vec<f64>,
+    rows: usize,
+    cols: usize,
+    /// row index at which each effector note starts
+    offsets: Vec<usize>,
+    active_joints: Vec<bool>,
+    active_points: Vec<bool>,
+}
+
+impl VecJacobian {
+    pub fn new() -> Self {
+        Self { ..Default::default() }
+    }
+}
+
+impl<T, R, I> Differentiable<T, R, I> for VecJacobian
+where
+    T: TreeIterable<R, I>,
+    R: Rigid,
+    I: Eq + Clone + Hash,
+{
+    type Data = Vec<f64>;
+
+    fn jacobian(&self) -> &Self::Data {
+        todo!()
+    }
+
+    fn setup(&mut self, tree: &T, active_joints: &HashSet<I>, active_points: &HashSet<I>) {
+        self.active_joints = tree
+            .iter(DepthFirst, None)
+            .map(|n| active_joints.get(&n.id()).is_some())
+            .collect();
+        self.active_points = tree
+            .iter(DepthFirst, None)
+            .map(|n| active_points.get(&n.id()).is_some())
+            .collect();
+        self.offsets = tree
+            .iter(DepthFirst, None)
+            .scan(0, |offset, node| {
+                let result = Some(*offset);
+                if active_points.get(&node.id()).is_some() {
+                    *offset += node.get().dim() * node.get().effector_count();
+                }
+                result
+            })
+            .collect();
+
+        self.rows = self.offsets.iter().sum();
+        self.cols = self.active_joints.iter().filter(|&active| *active).count();
+
+        self.data.clear();
+        self.data.resize(self.rows * self.cols, 0.0f64);
+    }
+
+    fn data(&mut self) -> &mut [f64] {
+        &mut self.data
+    }
+
+    fn rows(&self) -> usize {
+        self.rows
+    }
+
+    fn cols(&self) -> usize {
+        self.cols
+    }
+
+    fn compute(&mut self, tree: &T, params: &[<R as Rigid>::Parameter]) {
         // compute transformations only once
         let nodes_trafos = tree
             .iter(DepthFirst, None)
@@ -37,73 +105,50 @@ where
             .map(|(idx, (node, trafo))| (idx, node, trafo)) // flatten
             .collect_vec();
 
-        // I guess either column layout or the transpose if we want to go with the 3 trick
-        // now go with transpose
-        let (jacobian, m, n) = self.data(); // Array3::<f64>::zeros((m, n, 3));
-
-        // We need a good chunk by!
-
         // nomenclature (as in https://stats.stackexchange.com/a/588492): row-, column-, tube fibers
 
-        // TODO this does not jet respect multiple axes!
-        jacobian
+        self.data
             // .iter_mut()
-            .chunks_mut(n * 3) // TODO let the 3 be determined by the nodes and do a manual `chunk_by`
-            // .chunk_by_mut(|_, _| true)
+            .chunks_mut(self.rows)
             // TODO use rayon: into_par_iter()
-            // something wrong here
-            // .into_iter()
             .zip(
                 nodes_trafos
                     .iter()
-                    .zip(active_joints.iter()) // Add the active joint lists
+                    .zip(self.active_joints.iter()) // Add the active joint lists
                     .filter_map(|(x, active)| if *active { Some(x) } else { None }), // filter inactive joints and remove flag
                                                                                      // .par_iter()
             )
-            /* .Initially I wanted to iterate over the degrees of freedom, however each
-            axis should have its own `trafo` including the previous axis's tranformation
-            this makes it much more complex than anticipated.
-            Rather handle this internally. E.g. NodeID being a tuple data type of <string,usize> and implement hasfunction for this
-            map(|(idx, node, trafo)| ()),
-            That works! (surprisingly enough)
-            However, a 2D spline joint would be difficult to achieve (but doable with tuples of float as parameter type and a bit of duplication)
-            */
-            .for_each(|(row, (idx, joint_node, joint_trafo))| {
+            .for_each(|(col, (idx, joint_node, joint_trafo))| {
                 izip!(
                     tree.iter(DepthFirst, Some(joint_node)), // iterating over the child tree
-                    // zipping the corresponding trafos (by skipping until the current node)
+                    // zipping the corresponding trafos (by skipping until the current node) and the offsets in the column
                     // Using the index here is ok, keeping an iterator is to hard (gets mutated in a different closure)
                     nodes_trafos.iter().skip(*idx).map(|(_, _, trafo)| trafo),
-                    row.chunks_mut(3).into_iter(),
-                    active_points.iter()
+                    self.offsets.iter().skip(*idx),
+                    self.active_points.iter()
                 )
                 .filter(|(_, _, _, active)| **active)
-                .for_each(|(point_node, point_trafo, tube, _)| {
-                    // joint_trafo^{-1} * point_trafo is the argument to compute the displacement
-                    // Assign to slice with
-
-                    point_node.get().partial_derivative(joint_trafo, point_trafo, tube);
-                    // Or assign
-                    // tube.iter_mut().zip(vec![0f64; 3]).for_each(|(a, b)| *a = b);
+                .for_each(|(point_node, point_trafo, offset, _)| {
+                    // Todo, store in node (optimization)
+                    let size = point_node.get().effector_count() * point_node.get().dim();
+                    // The slice of the colum is itself a column-first matrix
+                    point_node
+                        .get()
+                        .partial_derivative(joint_trafo, point_trafo, &mut col[*offset..*offset + size]);
                 });
             });
     }
-
-    /// provide a reference to the raw data and the dimensions of its axes (so first the major axis)
-    fn data(&mut self) -> (&mut [f64], usize, usize);
-
-    // fn set(&mut self, data: &mut [f64], m: usize, n: usize )
 }
 
 #[cfg(feature = "ndarray")]
 mod ndarray {
-    use super::Differentiable;
+    use super::{Differentiable, VecJacobian};
     use crate::{Rigid, TreeIterable};
     use ndarray::Array2;
-    use std::hash::Hash;
+    use std::{collections::HashSet, hash::Hash};
 
-    struct NDArrayJacobian {
-        data: Array2<f64>,
+    pub struct NDArrayJacobian {
+        base: VecJacobian,
     }
 
     impl<T, R, I> Differentiable<T, R, I> for NDArrayJacobian
@@ -116,13 +161,28 @@ mod ndarray {
         type Data = Array2<f64>;
 
         fn jacobian(&self) -> &Self::Data {
-            &self.data
+            todo!()
         }
 
-        fn data(&mut self) -> (&mut [f64], usize, usize) {
-            let shape = self.data.shape();
-            let (m, n) = (shape[0], shape[1]);
-            (self.data.as_slice_mut().unwrap(), m, n)
+        fn data(&mut self) -> &mut [f64] {
+            // (self.base as dyn Differentiable<T, R, I, Data = Vec<f64>>).data()
+            <VecJacobian as Differentiable<T, R, I>>::data(&mut self.base)
+        }
+
+        fn setup(&mut self, tree: &T, active_joints: &HashSet<I>, active_points: &HashSet<I>) {
+            <VecJacobian as Differentiable<T, R, I>>::setup(&mut self.base, tree, active_joints, active_points)
+        }
+
+        fn rows(&self) -> usize {
+            todo!()
+        }
+
+        fn cols(&self) -> usize {
+            todo!()
+        }
+
+        fn compute(&mut self, tree: &T, params: &[<R as Rigid>::Parameter]) {
+            <VecJacobian as Differentiable<T, R, I>>::compute(&mut self.base, tree, params)
         }
     }
 }
